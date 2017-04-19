92355f19e431d8eb74138ac14b4ac13a8b588fe2
Tong Sun <suntong@cpan.org>
2017-04-19 16:30:55 -0400

    - [*] use the updated cc-pinyin-range package

--- cc2pyMain.go
+++ cc2pyMain.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-    "fmt"
-    pybr "github.com/go-cc/cc-table/cc-pinyin-range"
+	"fmt"
+	pybr "github.com/go-cc/cc-table/cc-pinyin-range"
 )
 
 func main() {
-    s := `名著：《红楼梦》〖清〗曹雪芹 著、高鹗 续／『人民文学』出版社／1996—9月30日／59.70【元】，《三国演义》〖明〗罗贯中。`
+	s := `名著：《红楼梦》〖清〗曹雪芹 著、高鹗 续／『人民文学』出版社／1996—9月30日／59.70【元】，《三国演义》〖明〗罗贯中。`
 
-    py := pybr.New()
-    p, _ := py.Convert(s)
+	py := pybr.New("")
+	p := py.Convert(s)
 
-    fmt.Println(p)
- 
+	fmt.Println(p)
 
-    //设置分隔符
-    //首字母是否大写
-    py.Split = "-"
-    py.Upper = false
-    p, _ = py.Convert(s)
-    fmt.Println(p)
+	//设置分隔符
+	//首字母是否大写
+	py.Split = "-"
+	py.Upper = false
+	p = py.Convert(s)
+	fmt.Println(p)
 }
