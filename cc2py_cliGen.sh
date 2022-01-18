a1438c66aeaf1b0423e4e317d22bc797d3eccb1b
Tong Sun <suntong@cpan.org>
2022-01-17 23:03:06 -0500

    - [#] switch code gen from cli to go-flags

--- cc2py_cliGen.sh
+++ cc2py_cliGen.sh
@@ -1,1 +1,3 @@
- easygen -tf ../../suntong/cli/cli-tx cc2py | gofmt > cc2pyCLIDef.go
+ pwd=`pwd`
+ cd $GOPATH/src/github.com/go-easygen/easygen/test
+ easygen commandlineGoFlags.header,commandlineGoFlags.ityped.tmpl,commandlineGoFlags "$pwd/cc2py"_cli | gofmt > "$pwd/cc2py"_cliDef.go
